36cee26d3bd363058735a9dc361f712b8f4cfe9c
Nicolas Ines <feryaz0122@gmail.com>
2023-09-26 10:16:41 -0300

    Design principles document activity

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,12 @@
+// Declaring and instantiating a variable to hold the current year
+const currentYear = new Date().getFullYear();
+
+// Placing the value of the current year variable into the HTML file
+document.getElementById('year').textContent = currentYear;
+
+// Declaring and instantiating a variable to hold the last modification
+let oLastModif = new Date(document.lastModified);
+
+// Placing the value of the current modification variable into the HTML file
+document.getElementById('modified').innerHTML = oLastModif;
+
