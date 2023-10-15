5d608ddd65ddf12785bf876c94e5236b4029a5c5
Nicolas Ines <feryaz0122@gmail.com>
2023-10-15 01:16:53 -0300

    BOM Top 10

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+	if (input.value != "") {
+		const li = document.createElement("li");
+		const deleteButton = document.createElement("button");
+		li.textContent = input.value;
+		deleteButton.textContent = "❌";
+		li.append(deleteButton);
+		list.append(li);
+		deleteButton.addEventListener("click", () => {
+			list.removeChild(li);
+			input.focus();
+		});
+        input.value = "";
+	} else {
+		const li = document.createElement("li");
+		li.textContent = "Please enter your favorite book and chapter.";
+		const deleteButton = document.createElement("button");
+		deleteButton.textContent = "↩";
+		li.append(deleteButton);
+		list.append(li);
+		deleteButton.addEventListener("click", () => {
+			list.removeChild(li);
+			input.focus();
+		});
+	}
+});
