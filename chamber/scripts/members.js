763881bc8ee777db00ad22b9fcf6e398d74895df
Nicolas Ines <feryaz0122@gmail.com>
2023-11-14 10:15:50 -0300

    directory page

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,37 +1,37 @@
 
 const baseUrl = "https://nines91.github.io/wdd230/";
-const linksURL = "https://nines91.github.io/wdd230/data/links.json";
-const cards = document.querySelector('#member-cards');
+const linksURL = "https://nines91.github.io/wdd230/chamber/data/member.json";
+const cards = document.querySelector('#members-card');
 
 async function getMembersData () {
-    const response = await fetch(url);
+    const response = await fetch(linksURL);
     const data = await response.json();
     console.table(data.companies);
-    /*displayProphets(data.prophets);*/
+    displayCompanies(data.companies);
 }
 
-/*const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
-      let fullName = document.createElement('h2'); 
+      let name = document.createElement('h2'); 
       let portrait = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name
-      fullName.textContent = `${prophet.name} ${prophet.lastname} `; 
+      name.textContent = `${company.name}`; 
       // Build the image portrait by setting all the relevant attributes
-      portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.fullName}`); 
+      portrait.setAttribute('src', company.imagIcon);
+      portrait.setAttribute('alt', `Portrait of ${company.name}`); 
       portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
+      portrait.setAttribute('width', '280');
+      portrait.setAttribute('height', '158');
   
       // Append the section(card) with the created elements
-      card.appendChild(fullName); 
+      card.appendChild(name); 
       card.appendChild(portrait);
   
       cards.appendChild(card);
     }); // end of arrow function and forEach loop
-  }*/
+  }
 
   getMembersData();
